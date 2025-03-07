060816e9c6177e70c500af334e0e8dcc6baba19e
HEMANTH <163557351+hemanth8705@users.noreply.github.com>
2025-03-07 14:50:33 +0530

    lavdalo gola

--- index.js
+++ index.js
@@ -11,7 +11,13 @@ const authRoutes = require("./routes/auth");
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+
+app.use(cors({ 
+  origin: process.env.FRONTEND_URL || "http://localhost:3000", 
+  credentials: true 
+}));
+
+
 app.use(express.json());
 app.use(
   session({ secret: "your_secret", resave: false, saveUninitialized: true })
@@ -33,6 +39,6 @@ mongoose
     console.error("MongoDB connection error:", err);
     process.exit(1);
   });
-  
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
